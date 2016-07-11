7a17cdb4d27a37b0d23ef2fa396c06d3f637e4bd
dubem <dubem@dubemenyekwe.com>
2016-07-10 20:49:54 -0500

    Add documentation for Attributes

--- Model/Attributes/DbTableAttribute.cs
+++ Model/Attributes/DbTableAttribute.cs
@@ -2,16 +2,30 @@
 
 namespace DamnORM.Model.Attributes
 {
+    /// <summary>
+    /// Attribute for database table information
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
     public sealed class DbTableAttribute : Attribute
     {
+        /// <summary>
+        /// The _ table name
+        /// </summary>
         private readonly string _TableName;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DbTableAttribute"/> class.
+        /// </summary>
+        /// <param name="tableName">Name of the table.</param>
         public DbTableAttribute(string tableName)
         {
             this._TableName = tableName;
         }
 
+        /// <summary>
+        /// Gets the name of the table.
+        /// </summary>
         public string TableName
         {
             get { return _TableName; }
